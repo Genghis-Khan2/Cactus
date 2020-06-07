906c35bea6a2606d368983e7905ea2bb72f44847
Genghis Khan <gkomet@g.jct.ac.il>
2020-06-07 18:20:39 +0300

    Added properties to ip_address_range and port_range

---
+++ ip_address_range.py
@@ -0,0 +1,85 @@
+from parse import parse, compile
+
+
+class ip_address_range(object):
+
+    def __init__(self):
+        self.__start_address="0.0.0.0"
+        self.__end_address="0.0.0.0"
+
+
+#region start_address property
+    @property
+    def start_address(self):
+        return self.__start_address
+
+
+    @start_address.setter
+    def start_address(self, value):
+        #TODO: Check properly for ip address structure
+        self.__start_address=value
+#endregion
+#region end_address property
+    @property
+    def end_address(self):
+        return self.__end_address
+
+
+    @end_address.setter
+    def start_address(self, value):
+        #TODO: Check properly for ip address structure
+        self.__end_address=value
+#endregion
+#region single_address property
+    @property
+    def single_address(self)
+        return self.start_address if self.start_address == self.end_address else None
+
+
+    @single_address.setter
+    def single_address(self, value):
+        self.start_address = self.end_address = value
+#endregion
+#region tuple_address property
+    @property
+    def tuple_address(self):
+        return (self.start_address, self.end_address)
+
+
+    @tuple_address.setter
+    def tuple_address(self, value):
+        #TODO: Make validity check
+        self.start_address, self.end_address = value
+#endregion
+
+    def __contains__(self, address):
+        p = compile("{a}.{b}.{c}.{d}")
+
+        result = p.parse(self.end_address)
+        end_1 = result['a']
+        end_2 = result['b']
+        end_3 = result['c']
+        end_4 = result['d']
+
+        result = p.parse(self.start_address)
+        start_1 = result['a']
+        start_2 = result['b']
+        start_3 = result['c']
+        start_4 = result['d']
+
+        result = p.parse(self.address)
+        address_1 = result['a']
+        address_2 = result['b']
+        address_3 = result['c']
+        address_4 = result['d']
+
+        return (start_1<=address_1<=end_1 and
+            start_2 <= address_2<=end_2 and
+            start_3 <= address_3 <= end_3 and
+            start_4 <= address_4 <= end_4)
+
+    
+    def __str__(self):
+        return (self.single_address 
+            if self.single_address 
+            else f"{self.start_address} - {self.end_address}")
---
+++ port_range.py
@@ -0,0 +1,62 @@
+class port_range(object):
+
+    def __init__(self):
+        self.__start_port=0
+        self.__end_port=0
+
+
+#region start_port property
+    @property
+    def start_port(self):
+        return self.__start_port
+
+
+    @start_port.setter
+    def start_port(self, value):
+        if 0 <= value <= 65535:
+            self.__start_port = value
+        #TODO: else throw exception
+#endregion
+#region end_port property
+    @property
+    def end_port(self):
+        return self.__end_port
+
+
+    @end_port.setter
+    def end_port(self, value):
+        if 0 <= value <= 65535:
+            self.__end_port = value
+        #TODO: Raise exception
+#endregion
+#region single_port property
+    @property
+    def single_port(self):
+        return self.start_port if self.start_port == self.end_port else None
+
+    
+    @single_port.setter
+    def single_port(self, value):
+        if 0 <= value <= 65535:
+            self.start_port = self.end_port = value
+        #TODO: Raise exception
+#endregion
+#region tuple_port property
+    @property
+    def tuple_port(self):
+        return (self.start_port, self.end_port)
+
+
+    @tuple_port.setter
+    def tuple_port(self, value):
+        #TODO: Check validity
+        self.start_port, self.end_port = value
+#endregion
+
+
+    def __str__(self):
+        return self.single_port if self.single_port else f"{self.start_port} - {self.end_port}"
+
+
+    def __contains__(self, port):
+        return self.start_port <= port <= self.end_port
